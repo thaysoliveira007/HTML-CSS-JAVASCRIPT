3d026f031a9807bc122445b477693b75744286f7
thaysoliveira007 <108279660+thaysoliveira007@users.noreply.github.com>
2024-11-07 08:05:27 -0300

    contador

---
+++ script.js
@@ -0,0 +1,11 @@
+let count=0;
+
+function increase(){
+    count++;
+    document.getElementById('counter').innerText=count;
+}
+
+function decrease(){
+    count--;
+    document.getElementById('counter').innerText=count;
+}
